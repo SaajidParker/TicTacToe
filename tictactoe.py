0acb70e8c73618edbadbe6f6a1f31e04f1ebc7e7
Saajid Parker <parkersaajid2@gmail.com>
2024-02-10 00:03:43 +0200

    Finishing up next turn function

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,36 @@ from tkinter import *
 import random
 
 def next_turn(row, column):
-    pass
+    global player
+
+    if buttons[row][column]['text'] == "" and check_winner() is False:
+        
+        if player == players[0]:
+
+            buttons[row][column]['text'] = player
+
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=(players[1]+"'s turn"))
+            
+            elif check_winner() is True:
+                label.config(text=(players[0]+ " wins"))
+            
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
+        
+        else:
+            buttons[row][column]['text'] = player
+
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=(players[0]+"'s turn"))
+            
+            elif check_winner() is True:
+                label.config(text=(players[1]+ " wins"))
+            
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
 
 def check_winner():
     pass
