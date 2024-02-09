e38dfcd1256c9543d4cde0db8446bb21ddeb2940
Saajid Parker <parkersaajid2@gmail.com>
2024-02-09 23:55:07 +0200

    More changes

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import random
 
-def next_turn():
+def next_turn(row, column):
     pass
 
 def check_winner():
@@ -13,28 +13,41 @@ def empty_spaces():
 def new_game():
     pass
 
-#Creatiug a window
+#Creating a window
 window = Tk()
 
 #Window title
 window.title("Tic-Tac-Toe")
 
 #Creating Players
-players = ["X","O"]
+players = ["X", "O"]
 #Selecting starter player
 player = random.choice(players)
 
 #Buttons for the grid
-button = [[0,0,0],
-          [0,0,0],
-          [0,0,0]]
+buttons = []
+for _ in range(3):
+    row = []
+    for _ in range(3):
+        row.append(None)
+    buttons.append(row)
 
 #Label for player turn
-label = Label(text= player + "'s Turn", font =('consolas',40))
+label = Label(text= player + "'s Turn", font=('consolas', 40))
 label.pack(side="top")
 
 #Creating reset button
-reset_button = Button(text="Restart", font=('consolas',20), command=new_game)
+reset_button = Button(text="Restart", font=('consolas', 20), command=new_game)
 reset_button.pack(side="top")
+
+#Creating a frame for a window
+frame = Frame(window)
+frame.pack()
+
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="", font=('consolas', 40), width=5, height=2,
+                                      command=lambda row=row, column=column: next_turn(row, column))
+        buttons[row][column].grid(row=row, column=column)
 
 window.mainloop()
